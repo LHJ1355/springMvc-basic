358f83db32eb1267ad4ba0c2f06301f1c5d5c15e
LHJ1355 <lhj56411355@gmail.com>
2021-07-21 22:51:34 +0900

    template

---
+++ src/main/java/hello/springmvc/controller/BasicItemController.java
@@ -0,0 +1,25 @@
+package hello.springmvc.controller;
+
+import hello.springmvc.domain.Item;
+import hello.springmvc.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+    private final ItemRepository itemRepository;
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+}
