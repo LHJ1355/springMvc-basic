d6401a289d6c57ecdebc2dbc24ffe7b9e97edd2f
LHJ1355 <lhj56411355@gmail.com>
2021-07-24 22:14:01 +0900

    bean validation

--- src/main/java/hello/springmvc/domain/Item.java
+++ src/main/java/hello/springmvc/domain/Item.java
@@ -1,13 +1,26 @@
 package hello.springmvc.domain;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 
 @Data
 public class Item {
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
+
+    @NotBlank(message = "공백은 입력할 수 없습니다. {0}")
     private String itemName;
+
+    @NotNull
+    @Range(message = "범위는 {1}에서 {2}사이여야 합니다.",min = 1000, max = 100000)
     private Integer price;
+    @NotNull
+    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
@@ -19,4 +32,8 @@ public class Item {
         this.price = price;
         this.quantity = quantity;
     }
+
+    public interface SaveCheck {}
+    public interface UpdateCheck {}
+
 }
--- src/main/java/hello/springmvc/validation/ValidationItemController.java
+++ src/main/java/hello/springmvc/validation/ValidationItemController.java
@@ -6,12 +6,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.SmartValidator;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.groups.Default;
 
 @Controller
 @Slf4j
@@ -19,22 +20,52 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class ValidationItemController {
     private final ItemValidator itemValidator;
     private final ItemRepository itemRepository;
+    private final SmartValidator validator;
 
-    @InitBinder
-    public void init(WebDataBinder dataBinder) {
-        log.info("init binder {}", dataBinder);
-        dataBinder.addValidators(itemValidator);
-    }
+//    @InitBinder
+//    public void init(WebDataBinder dataBinder) {
+//        log.info("init binder {}", dataBinder);
+//        dataBinder.addValidators(itemValidator);
+//    }
 
-    @PostMapping("/v1/add")
-    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult,
+//    @PostMapping("/v1/add")
+//    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult,
+//                            RedirectAttributes redirectAttributes) {
+//        ItemValidator.validate(item, bindingResult);
+//        if (bindingResult.hasErrors()) {
+//            log.info("errors={}", bindingResult);
+//            return "validation/v2/addForm";
+//        }
+//
+//        //성공 로직
+//        Item savedItem = itemRepository.save(item);
+//        redirectAttributes.addAttribute("itemId", savedItem.getId());
+//        redirectAttributes.addAttribute("status", true);
+//        return "redirect:/validation/v2/items/{itemId}";
+//    }
+//
+//    @PostMapping("/v2/add")
+//    public String addItemV2(@Validated @ModelAttribute Item item, BindingResult bindingResult,
+//                            RedirectAttributes redirectAttributes) {
+//        if (bindingResult.hasErrors()) {
+//            log.info("errors={}", bindingResult);
+//            return "validation/v2/addForm";
+//        }
+//
+//        //성공 로직
+//        Item savedItem = itemRepository.save(item);
+//        redirectAttributes.addAttribute("itemId", savedItem.getId());
+//        redirectAttributes.addAttribute("status", true);
+//        return "redirect:/validation/v2/items/{itemId}";
+//    }
+
+    @PostMapping("/v3/add")
+    public String addItemV3(@Validated @ModelAttribute Item item, BindingResult bindingResult,
                             RedirectAttributes redirectAttributes) {
-        ItemValidator.validate(item, bindingResult);
         if (bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
             return "validation/v2/addForm";
         }
-
         //성공 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
@@ -42,18 +73,19 @@ public class ValidationItemController {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/v2/add")
-    public String addItemV2(@Validated @ModelAttribute Item item, BindingResult bindingResult,
+    @PostMapping("/v4/add")
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult,
                             RedirectAttributes redirectAttributes) {
+        validator.validate(item, bindingResult, Item.SaveCheck.class, Default.class);
         if (bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
             return "validation/v2/addForm";
         }
-
         //성공 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
     }
+
 }
