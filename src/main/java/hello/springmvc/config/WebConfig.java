89ca7adeb5f7cd1cc157cab09ac4c3e7f9323e03
LHJ1355 <lhj56411355@gmail.com>
2021-07-28 22:02:36 +0900

    interceptor

--- src/main/java/hello/springmvc/config/WebConfig.java
+++ src/main/java/hello/springmvc/config/WebConfig.java
@@ -2,14 +2,32 @@ package hello.springmvc.config;
 
 import hello.springmvc.filter.LogFilter;
 import hello.springmvc.filter.LoginCheckFilter;
+import hello.springmvc.interceptor.LogInterceptor;
+import hello.springmvc.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error")
+                .order(1);
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error")
+                .order(2);
+    }
 
     @Bean
     public FilterRegistrationBean logFilter() {
